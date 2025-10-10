b8175f99099803ff99bccb22d06ec0fc2d43e002
Pranjal-ux <pranjal2shukla@gmail.com>
2025-10-10 20:08:23 +0530

    #Lec-06 #Done

--- Beta.js
+++ Beta.js
@@ -1,25 +1,25 @@
-const express = require('express');
+// const express = require('express');
 
-const app = express();
+// const app = express();
 
-app.use(function (req, res, next) {
-    console.log("Middleware chala");
-    next();
-});
-app.use(function (req, res, next) {
-    console.log("Middleware phirse chala");
-    next();
-});
+// app.use(function (req, res, next) {
+//     console.log("Middleware chala");
+//     next();
+// });
+// app.use(function (req, res, next) {
+//     console.log("Middleware phirse chala");
+//     next();
+// });
 
 
-app.get("/", function (req, res) {
-    res.send("First routing done and we update the value")
-})
-app.get("/about", function (req, res, next) {
-    return next(new Error("something went wrong"))
-})
-app.use((err, req, res, next) => {
-    console.error(err.stack)
-    res.status(500).send('Something broke!')
-})
-app.listen(3001);
+// app.get("/", function (req, res) {
+//     res.send("First routing done and we update the value")
+// })
+// app.get("/about", function (req, res, next) {
+//     return next(new Error("something went wrong"))
+// })
+// app.use((err, req, res, next) => {
+//     console.error(err.stack)
+//     res.status(500).send('Something broke!')
+// })
+// app.listen(3001);
