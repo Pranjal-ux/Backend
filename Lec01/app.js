d953724b84c53337bef01a201816fd9a6d8c7e9c
Pranjal-ux <pranjal2shukla@gmail.com>
2025-11-20 10:16:42 +0530

    FsModule

--- Lec01/app.js
+++ Lec01/app.js
@@ -7,12 +7,41 @@ const fs = require('fs')
 
 //apend=>means adding something next to the write file
 
-fs.appendFile("hey.txt", " sab badhiya", function (err) {
+// fs.appendFile("hey.txt", " sab badhiya", function (err) {
+//     if (err) console.log(err)
+//     else console.log("Done")
+// })
+
+// fs.rename("hey.txt", "hello.txt", function (err) {
+//     if (err) console.log(err)
+//     else console.log("done")
+// })
+fs.copyFile("hello.txt", "./Copy/Hallo.txt", function (err) {
+    if (err) console.log(err)
+    else console.log("Done ")
+})
+fs.unlink("hello.txt", function (err) {
     if (err) console.log(err)
     else console.log("Done")
 })
 
-fs.rename("hey.txt", "hello.txt", function (err) {
+fs.rmdir('./Copy', function (err) {
     if (err) console.log(err)
-    else console.log("done")
+    else console.log("Done")
+})
+fs.rm('./Copy', { recursive: true }, function (err) {
+    if (err) console.log(err)
+    else console.log("Done")
+})
+fs.mkdir('Create', function (err) {
+    if (err) console.log(err)
+    else console.log("Create")
+})
+fs.readFile("hey.txt", function (err) {
+    if (err) console.log(err)
+    else console.log("ReadingDone")
+})
+fs.rm('./Create', { recursive: true }, function (err) {
+    if (err) console.log(err)
+    else console.log("Done")
 })
