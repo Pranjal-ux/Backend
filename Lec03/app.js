1f789fde3717166a63c76cc697a2106061245bfa
Pranjal-ux <pranjal2shukla@gmail.com>
2025-11-22 20:22:45 +0530

    #Dynamic Routing #Lec03 Done

--- Lec03/app.js
+++ Lec03/app.js
@@ -1,12 +1,18 @@
-const express = require('express')
+const express = require('express') //This line we setup our basic express app
+const path = require('path') // path is use for completing the dependices
 const app = express()
+
+//This two line use for form authenticarion
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.set("view engine", "ejs")
-app.use(express.static(path.join(___dirname, 'public')))
-app.get("/", function (req, res) {
+app.set("view engine", "ejs") // This line help us rendening the ejs pages
+app.use(express.static(path.join(__dirname, 'public')))// This line help us to use staic-file like images,fonts,video etc....
+app.get("/", function (req, res) { // this is basic route
     res.render("index")
 })
-app.listen(3000)
-// const path = require('path')
-// console.log(path.join(__dirname, "public"))
+app.get("/profile/:username", function (req, res) { // this is Dynamic route
+    res.send(`welcome,${req.params.username}`)
+})
+app.listen(3000) // this is the basic localHost server
+
+
